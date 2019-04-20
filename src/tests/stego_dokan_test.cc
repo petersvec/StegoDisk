4f7a41bdc4d9b41541c9cfcc502c8707c7856ad1
petersvec <32421639+petersvec@users.noreply.github.com>
2019-04-20 21:51:16 +0200

    Dokan test app

--- src/tests/stego_dokan_test.cc
+++ src/tests/stego_dokan_test.cc
@@ -6,32 +6,139 @@
 #include "fuse/dokan_service.h"
 #include "logging/logger.h"
 
-int main(int argc, char *argv[])
+std::unique_ptr<stego_disk::StegoStorage> stego_storage{ nullptr };
+
+void help()
 {
-	Logger::SetVerbosityLevel(std::string("DEBUG"), std::string("cout"));
+	std::cout << "Usage: stego_dokan_test.exe <option(s)>" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "\t-h,--help\t\tShow this help message" << std::endl;
+	std::cout << "\t-d,--directory\t\tSpecitfy the source directory" << std::endl;
+	std::cout << "\t-p,--password\t\tSpecify if the password should be used" << std::endl;
+	std::cout << "\t-c,--config\t\tSpecify the configuration file" << std::endl;
+}
 
-	LOG_INFO("Start...");
+void commands_help()
+{
+	std::cout << "help\t\tShow this help message" << std::endl;
+	std::cout << "mount\t\tMount virtualdisc.iso file" << std::endl;
+	std::cout << "unmount\t\tUnmount virtualdisc.iso file and exit" << std::endl;
+} 
 
-	auto stego_storage = std::make_unique<stego_disk::StegoStorage>();
-	stego_storage->Configure("C:\\Users\\Peter\\Desktop\\diplomovka\\config.json");
-	stego_storage->Open("C:\\Users\\Peter\\Desktop\\diplomovka\\test_dir", "heslo");
-	stego_storage->Load();
+void mount_virtualdisc()
+{
+	system("powershell.exe Mount-DiskImage -ImagePath 'M:\\virtualdisc.iso'");
+}
 
-	std::cout << "Size: " << stego_storage->GetSize() << std::endl;
+void unmount_virtualdisc()
+{
+	system("powershell.exe Dismount-DiskImage -ImagePath 'M:\\virtualdisc.iso'");
+}
 
+void mount_cmd()
+{
+	while (true)
+	{
+		std::string command;
+		std::cout << "Command: ";
+		std::cin >> command;
+
+		if (command == "help")
+		{
+			commands_help();
+		}
+		else if (command == "mount")
+		{
+			mount_virtualdisc();
+		}
+		else if (command == "unmount")
+		{
+			unmount_virtualdisc();
+			stego_storage->Save();
+			stego_disk::DokanService::Unmount();
+		}
+	}
+}
+
+void mount()
+{
+	std::cout << "Mounting..." << std::endl;
 	stego_disk::DokanService::Init(stego_storage.get(), "M:\\");
 	stego_disk::DokanService::Mount();
-
-	std::cout << "Mounting..." << std::endl;
 	stego_disk::DokanService::IsMounted();
 	std::cout << "Mounted..." << std::endl;
+}
 
-	std::this_thread::sleep_for(std::chrono::seconds(50));
+void init_stego_storage(const std::string &directory, const std::string &password, const std::string &config)
+{
+	stego_storage = std::make_unique<stego_disk::StegoStorage>();
+	stego_storage->Configure(config);
+	stego_storage->Open(directory, password);
+	stego_storage->Load();
 
-	//std::cout << "Unmounting...";
-	//stego_disk::DokanService::Unmount();
+	mount();
+	mount_cmd();
+}
 
-//	std::cout << "Mounting virtual disc..." << std::endl;
-//	system("powershell.exe Mount-DiskImage -ImagePath 'M:\\virtualdisc.iso'");
+int main(int argc, char *argv[])
+{
+	Logger::SetVerbosityLevel(std::string("INFO"), std::string("cout"));
+
+	std::string directory{ "" }, password{ "" }, config{ "" };
+
+	for (auto i = 1u; i < argc; ++i)
+	{
+		if (auto arg = std::string(argv[i]); arg == "-h" || arg == "--help")
+		{
+			help();
+			return 0;
+		}
+		else if (arg == "-d" || arg == "--directory")
+		{
+			if (++i < argc)
+			{
+				directory = argv[i];
+			}
+			else
+			{
+				std::cout << "--directory option requires one argument" << std::endl;
+				return -1;
+			}
+		}
+		else if (arg == "-p" || arg == "--password")
+		{
+			if (++i < argc)
+			{
+				password = argv[i];
+			}
+			else
+			{
+				std::cout << "--password option requires one argument" << std::endl;
+				return -1;
+			}
+		}
+		else if (arg == "-c" || arg == "--config")
+		{
+			if (++i < argc)
+			{
+				config = argv[i];
+			}
+			else
+			{
+				std::cout << "--config option requires one argument" << std::endl;
+			}
+		}
+	}
+
+	if (!directory.empty() && !config.empty())
+	{
+		init_stego_storage(directory, password, config);
+	}
+	else
+	{
+		std::cout << "No directory or configuration file specified" << std::endl;
+		return -1;
+	}
+
 	return 0;
 }
